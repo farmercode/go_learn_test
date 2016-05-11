9ce353357f892458b27ed10f17ec7664b5aabef3
king <wangchangchun120@gmail.com>
2016-05-11 12:11:26 +0800

    添加range遍历字符串

--- chapter2/string_each.go
+++ chapter2/string_each.go
@@ -7,8 +7,14 @@ func main() {
 	//注意中文字符在utf8中占3个字节
 	str1 := "abcdefg牛气冲天"
 	len := len(str1)
+	//下标遍历字符串方式
 	for i :=0; i < len; i++ {
 		ch := str1[i]
 		fmt.Println(i,ch)
+	}
+
+	//range遍历字符串方式
+	for pos,c := range str1 {
+		fmt.Println(pos,c)
 	}
 }
