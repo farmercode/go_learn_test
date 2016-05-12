00f1a26476024d36100e466c2660006ec88924d8
king <wangchangchun120@gmail.com>
2016-05-12 15:56:18 +0800

    加入copy例子

--- chapter2/slice_demo.go
+++ chapter2/slice_demo.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main()  {
-	mySlice := make([]int,5,10)
+	mySlice := make([]int,5,20)
 	mySlice1 := []int{11,12,13}
 
 	fmt.Println("mySlice : ", mySlice)
@@ -11,6 +11,9 @@ func main()  {
 	mySlice = append(mySlice,1,2,3)
 	fmt.Println("mySlice : ", mySlice)
 	mySlice = append(mySlice,mySlice1...)
+	fmt.Println("mySlice : ", mySlice)
+	//复制mySlice1中的所有元素到mySlice中对应的位置
+	copy(mySlice,mySlice1)
 	fmt.Println("mySlice : ", mySlice)
 
 	fmt.Println("len(mySlice) : " ,len(mySlice))
